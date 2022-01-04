3b052d7acdfa7fedfebbf359f8629ede44d77877
AlexandrePDA <a.pires130@gmail.com>
2022-01-04 18:36:58 +0100

    fetch

--- front/js/script.js
+++ front/js/script.js
@@ -1,14 +1,19 @@
 let dataKanap = [];
 
+// fetch API Kanap, dataKanap posséde les données de l'API
+// kanapItem correspond maintenant à chaque canapé
 const fetchKanap = async() => {
     await fetch('http://localhost:3000/api/products')
     .then((response) => response.json())
     .then((data) => {
-        dataKanap = data;
-        console.log(dataKanap);
+        dataKanap = data
     })
-};
+    
+    for(const kanapItem of dataKanap){
+            console.log(kanapItem);
+        }
+    };
+
 
 fetchKanap()
-
 
